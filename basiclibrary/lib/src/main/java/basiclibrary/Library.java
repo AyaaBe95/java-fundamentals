d11aeb0960be3b0d8cbb12300651f4a5be68d2d5
aya berjawi <ayabarjawi@gmail.com>
2021-04-06 23:17:06 +0300

    lab3

--- basiclibrary/lib/src/main/java/basiclibrary/Library.java
+++ basiclibrary/lib/src/main/java/basiclibrary/Library.java
@@ -11,20 +11,23 @@ public class Library {
     }
 
     public static void main(String[] args) {
-        //  calculateAverage(array);
-        //roll(4);
-        // System.out.print(containsDuplicates(array));
 
-//        int[][] array = {
-//                {66, 64, 58, 65, 71, 57, 60},
-//                {57, 65, 65, 70, 72, 65, 51},
-//                {55, 54, 60, 53, 59, 57, 61},
-//                {65, 56, 55, 52, 55, 62, 57}
-//        };
-//
-//        calculateAverageOfTwoArrays(array);
+        int []array1 = {1,2,3,1}
 
-//        System.out.println(AnalyzingWeatherData(array));
+          calculateAverage(array1);
+        roll(4);
+         System.out.print(containsDuplicates(array1));
+
+        int[][] array = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+
+        calculateAverageOfTwoArrays(array);
+
+        System.out.println(AnalyzingWeatherData(array));
 
         List<String> votes = new ArrayList<>();
         votes.add("Bush");
